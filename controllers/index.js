db16c299b77cf19bf84fd31fecb111f8657e465a
Juan Santos <62819163+thereal-baitjet@users.noreply.github.com>
2021-07-11 12:50:45 -0400

    established routes for user posts and comments

---
+++ controllers/index.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
+
+// Define the path for the server for the API routes
+router.use('/api', apiRoutes);
+
+// Define the path for the home page
+router.use('/', homeRoutes);
+
+// Define the path for the dashboard
+router.use('/dashboard', dashboardRoutes);
+
+// Define a catch-all route for any resource that doesn't exist
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
